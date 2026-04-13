0ee1fdd78639b51e9b7ccd8738447a331f9a2cb0
Hermitxxx <cvenom477@gmail.com>
2026-04-13 23:47:59 +0600

    testing if the live server link works perfectly

---
+++ src/app/test/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h1 className="text-center text-4xl bg-blue-400 p-2">This is the test page</h1>
+        </div>
+    );
+};
+
+export default Test;
